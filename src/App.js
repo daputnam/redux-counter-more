52cba2751082a6e2da53248fdddcf42a815a560f
Dan Putnam <daniel.a.putnam@gmail.com>
2018-02-20 17:57:17 -0600

    app

--- src/App.js
+++ src/App.js
@@ -3,28 +3,8 @@ import './App.css';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Counter from './containers/Counter';
+import reducer from './reducers/reducer'
 
-const initialState = {
-  count: 0
-}
-
-function reducer(state = initialState, action) {
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        ...state,
-        count: state.count + 1
-      }
-    case "DECREMENT":
-      return {
-        ...state,
-        count: state.count - 1
-      }
-    default:
-      return state;
-  }
-
-}
 const store = createStore(reducer);
 
 
